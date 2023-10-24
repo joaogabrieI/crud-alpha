af22296fc7080d2cd76b249cf80283b2aed86f65
joaogabriei <joaonalves1@gmail.com>
2023-10-24 17:23:22 -0300

    unique index adm_email

--- src/login-cadastro/verificaCadastro.php
+++ src/login-cadastro/verificaCadastro.php
@@ -7,11 +7,10 @@ $email = filter_input(INPUT_POST, 'email');
 $senha = filter_input(INPUT_POST, 'senha');
 $senha = password_hash($senha, PASSWORD_DEFAULT);
 
+$sql = "INSERT INTO administrador (adm_nome, adm_email, adm_senha) VALUES (:nome, :email, :senha)";
+$stmt = $pdo->prepare($sql);
+
 try {
-    $sql = "INSERT INTO administrador (adm_nome, adm_email, adm_senha) VALUES (:nome, :email, :senha)";
-
-    $stmt = $pdo->prepare($sql);
-
     $stmt->bindParam(':nome', $nome, PDO::PARAM_STR);
     $stmt->bindParam(':email', $email, PDO::PARAM_STR);
     $stmt->bindParam(':senha', $senha, PDO::PARAM_STR);
