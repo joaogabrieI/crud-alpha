fec31f8f61b14519b66c13022a2669a46a5d8f58
joaogabriei <joaonalves1@gmail.com>
2023-10-31 13:46:34 -0300

    tabela do banco de dados atualizada

--- tabela-alpha.sql
+++ tabela-alpha.sql
@@ -35,6 +35,16 @@ CREATE TABLE `administrador` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `administrador`
+--
+
+LOCK TABLES `administrador` WRITE;
+/*!40000 ALTER TABLE `administrador` DISABLE KEYS */;
+INSERT INTO `administrador` VALUES (10,'jamel','jamela@jamela','$2y$10$tpq69ko3q7rTEUWgOvkN.e/m6PBUFfy2.NwrrhsoOjoccvJeEv7fC',NULL),(22,'jamelao','jamela@jamela.com','$2y$10$/C7lglT9mE.J0SZ2/z41rOBdQEettnY0w71G82QRbu.14WgNUnVkq',NULL),(25,'joao gabriel','joao@joao.com','$2y$10$HuisvJONNsPbRy.TMX9FAehCR2YyxLrc1tDxM7BBHGdjPrlaiznvi',NULL),(26,'gabriel','gabriel@gabriel.com','$2y$10$paqtp2wRNs3vQf5Pvok5dehfHLCSszir1tKvh0UAmE9F0Rn3JQPeS',NULL);
+/*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `categoria`
 --
 
@@ -51,6 +61,16 @@ CREATE TABLE `categoria` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+INSERT INTO `categoria` VALUES (1,'console','Computador dedicado para jogos',NULL),(3,'Jogos','Jogos para console',NULL),(4,'teste2','teste2',NULL);
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `produto`
 --
 
@@ -65,9 +85,20 @@ CREATE TABLE `produto` (
   `PRODUTO_DESCONTO` decimal(5,2) NOT NULL,
   `CATEGORIA_ID` int DEFAULT NULL,
   `PRODUTO_ATIVO` bit(1) DEFAULT NULL,
-  PRIMARY KEY (`PRODUTO_ID`)
+  PRIMARY KEY (`PRODUTO_ID`),
+  KEY `FK_CATEGORIA_CATEGORIA_PRODUTO_idx` (`CATEGORIA_ID`),
+  CONSTRAINT `FK_CATEGORIA_CATEGORIA_PRODUTO` FOREIGN KEY (`CATEGORIA_ID`) REFERENCES `categoria` (`CATEGORIA_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produto`
+--
+
+LOCK TABLES `produto` WRITE;
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `produto_estoque`
@@ -78,13 +109,47 @@ DROP TABLE IF EXISTS `produto_estoque`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `produto_estoque` (
   `PRODUTO_ID` int NOT NULL,
-  `PRODUTO_QTD` int NOT NULL
+  `PRODUTO_QTD` int NOT NULL,
+  KEY `FK_PRODUTO_PRODUTO_ESTOQUE_idx` (`PRODUTO_ID`),
+  CONSTRAINT `FK_PRODUTO_PRODUTO_ESTOQUE` FOREIGN KEY (`PRODUTO_ID`) REFERENCES `produto` (`PRODUTO_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping events for database 'alpha'
+-- Dumping data for table `produto_estoque`
 --
+
+LOCK TABLES `produto_estoque` WRITE;
+/*!40000 ALTER TABLE `produto_estoque` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produto_estoque` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produto_imagem`
+--
+
+DROP TABLE IF EXISTS `produto_imagem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produto_imagem` (
+  `IMAGEM_ID` int NOT NULL AUTO_INCREMENT,
+  `IMAGEM_ORDEM` int NOT NULL,
+  `PRODUTO_ID` int NOT NULL,
+  `IMAGEM_URL` varchar(8000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`IMAGEM_ID`),
+  KEY `FK_PRODUTO_PRODUTO_IMAGEM_idx` (`PRODUTO_ID`),
+  CONSTRAINT `FK_PRODUTO_PRODUTO_IMAGEM` FOREIGN KEY (`PRODUTO_ID`) REFERENCES `produto` (`PRODUTO_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produto_imagem`
+--
+
+LOCK TABLES `produto_imagem` WRITE;
+/*!40000 ALTER TABLE `produto_imagem` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produto_imagem` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -95,4 +160,4 @@ CREATE TABLE `produto_estoque` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-26 15:07:31
+-- Dump completed on 2023-10-31 10:41:33
