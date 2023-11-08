8ab8372b97fa4a45522c0cfaef27a50fa877a633
joaogabriei <joaonalves1@gmail.com>
2023-11-08 21:32:54 -0200

    ativo recebe 1 ao cadastrar

--- src/categoria/adicionaCategoria.php
+++ src/categoria/adicionaCategoria.php
@@ -7,7 +7,7 @@ require "../conexao-banco.php";
 $nome = filter_input(INPUT_POST, 'nomeCategoria');
 $descricao = filter_input(INPUT_POST, 'descricao');
 
-$sql = "INSERT INTO CATEGORIA (CATEGORIA_NOME, CATEGORIA_DESC) VALUES (:nome, :descricao)";
+$sql = "INSERT INTO CATEGORIA (CATEGORIA_NOME, CATEGORIA_DESC, CATEGORIA_ATIVO) VALUES (:nome, :descricao, 1)";
 $stmt = $pdo->prepare($sql);
 
 $stmt->bindParam(':nome', $nome, PDO::PARAM_STR);
