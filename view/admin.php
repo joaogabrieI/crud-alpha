1468a29527cc11af594fd858fc55aae1778de36e
joaogabriei <joaonalves1@gmail.com>
2023-11-09 11:50:32 -0300

    template html da página inicial do administrador

--- view/admin.php
+++ view/admin.php
@@ -22,51 +22,70 @@ $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/style.css">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../assets/style/adm.css" />
     <title>Admin</title>
+
 </head>
 
 <body>
     <header>
-
         <nav>
             <div class="logo">
-                <img id="logo" src="../assets/img/logo.png" alt="">
+                <img id="logo" src="../assets/img/logo.png" alt="" />
                 <p>Olá, Seja Bem-vindo!</p>
             </div>
             <p>Administração</p>
         </nav>
     </header>
+
     <p id="linha"></p>
     <p id="linhaVertical"></p>
 
-    <section class="acoes">
-        <ul>
-            <li> 
-                <a href="admin.php"><img src="../assets/img/house-icon.png" alt="">Inicio</a>
-            </li>
-            <li>
-                <a href="produtos.php"><img src="../assets/img/database-icon.png" alt="">Produtos</a>
-            </li>
-            <li>
-                <a href="categorias.php"><img src="../assets/img/tags-icon.png" alt="">Categorias</a>
-            </li>
-            <li>
-                <a href="usuarios.php"><img src="../assets/img/person-icon.png" alt="">Usuários</a>
-            </li>
-        </ul>
+    <main>
+
+        <section class="acoes">
+            <ul>
+                <li>
+                    <a href="admin.php"><img src="../assets/img/house-icon.png" alt="" />Inicio</a>
+                </li>
+                <li>
+                    <a href="produtos.php"><img src="../assets/img/database-icon.png" alt="" />Produtos</a>
+                </li>
+                <li>
+                    <a href="categorias.php"><img src="../assets/img/tags-icon.png" alt="" />Categorias</a>
+                </li>
+                <li>
+                    <a href="usuarios.php"><img src="../assets/img/person-icon.png" alt="" />Usuários</a>
+                </li>
+            </ul>
+        </section>
+
+        <section>
+
+            <div class="nav-adm-produto">
+
+                <img src="../assets/img/icone.png" alt="" class="icon-m">
+                <div class="dados">Novo Produto</div>
+                <div class="dados">Filtrar</div>
+
+            </div>
+
+        </section>
 
 
-    </section>
 
-    <?php foreach ($usuarios as $usuario): ?>
-    <section id="usuario">
-        <p id="nomeUsuario"><?= $usuario["ADM_NOME"]?></p>
-        <a href="../src/login-cadastro/logout.php">Sair</a>
-    </section>
+    </main>
+
+    <?php foreach ($usuarios as $usuario) : ?>
+        <footer>
+            <div id="usuario">
+                <p id="nomeUsuario"><?=$usuario['ADM_NOME']?></p>
+                <a href="../src/login-cadastro/logout.php">Sair</a>
+            </div>
+        </footer>
     <?php endforeach; ?>
 
 
