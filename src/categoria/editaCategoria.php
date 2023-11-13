545ed94f0f436e267878bf7528e9c48f639999af
joaogabriei <joaonalves1@gmail.com>
2023-11-13 11:30:02 -0300

    campo de categoria ativa ou inativa no formulário de edição de categoria

--- src/categoria/editaCategoria.php
+++ src/categoria/editaCategoria.php
@@ -8,11 +8,13 @@ $id = $_GET["id"];
 
 $nomeCategoria = filter_input(INPUT_POST, 'nomeCategoria');
 $descricao = filter_input(INPUT_POST, 'descricao');
+$ativo = filter_input(INPUT_POST, 'ativo');
 
-$sql = "UPDATE CATEGORIA SET CATEGORIA_NOME = :nomeCategoria, CATEGORIA_DESC = :descricao WHERE CATEGORIA_ID = :id";
+$sql = "UPDATE CATEGORIA SET CATEGORIA_NOME = :nomeCategoria, CATEGORIA_DESC = :descricao, CATEGORIA_ATIVO = :ativo WHERE CATEGORIA_ID = :id";
 $stmt = $pdo->prepare($sql);
 $stmt->bindParam(":nomeCategoria", $nomeCategoria, PDO::PARAM_STR);
 $stmt->bindParam(":descricao", $descricao, PDO::PARAM_STR);
+$stmt->bindParam(":ativo", $ativo, PDO::PARAM_STR);
 $stmt->bindParam(":id", $id, PDO::PARAM_STR);
 
 if($stmt->execute()){
