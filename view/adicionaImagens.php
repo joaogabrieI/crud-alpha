716be1013872066841c5b18d6eee0f421541ad13
joaogabriei <joaonalves1@gmail.com>
2023-11-22 16:13:55 -0300

    template html/css da parte de adicionar imagens

--- view/adicionaImagens.php
+++ view/adicionaImagens.php
@@ -1,7 +1,7 @@
-<?php 
-    session_start();
+<?php
+session_start();
 
-    $id = $_GET['id'];
+$id = $_GET['id'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,31 +10,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar Imagens</title>
+    <link rel="stylesheet" href="../assets/style/adicionaImagens.css">
 </head>
 
 <body>
     <main>
-        <section>
-            <!-- products.php - Página que exibe detalhes do produto e permite adicionar novas imagens -->
-            <form action="../src/produto/addImagens.php?id=<?=$id?>" method="post" enctype="multipart/form-data">
-                <label for="imagem">Adicionar Novas Imagens:</label>
-                <input type="file" name="imagem[]" multiple accept="image/*" required>
+        <section id="add-imagem">
+            <div class="content">
+                <form action="../src/produto/addImagens.php?id=<?= $id ?>" method="post" enctype="multipart/form-data">
+                    <label for="imagem">Adicionar Novas Imagens:</label>
+                    <input type="file" name="imagem[]" multiple accept="image/*" required>
+                    <input type="submit" value="Adicionar Imagens" id="submit-add-img">
+                </form>
 
-                <input type="submit" value="Adicionar Imagens">
-            </form>
-
-            <p>
-                <?php
-                if (isset($_SESSION['msg'])) {
-                    echo $_SESSION['msg'];
-                    unset($_SESSION['msg']);
-                }
-                ?>
-            </p>
-
+                <p id="erro-msg">
+                    <?php
+                    if (isset($_SESSION['msg'])) {
+                        echo $_SESSION['msg'];
+                        unset($_SESSION['msg']);
+                    }
+                    ?>
+                </p>
+                <div id="botao-voltar">
+                    <a href="ordenaImagensForm.php?id=<?= $id ?>" class="botao-voltar-link">Voltar</a>
+                </div>
+            </div>
         </section>
-
-        <a href="ordenaImagensForm.php?id=<?=$id?>">Voltar</a>
     </main>
 </body>
 
