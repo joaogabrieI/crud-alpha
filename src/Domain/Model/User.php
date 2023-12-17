e0b3dfcf220684fab573a7a44b94459267ce1d2a
joaogabriei <joaonalves1@gmail.com>
2023-12-17 15:24:27 -0300

    ajustes classe user

--- src/Domain/Model/User.php
+++ src/Domain/Model/User.php
@@ -9,8 +9,9 @@ class User
     private string $email;
     private string $password;
     private int $active;
-    
-    public function __construct($id, $name, $email, $password, $active){
+
+    public function __construct($id, $name, $email, $password, $active)
+    {
         $this->id = $id;
         $this->name = $name;
         $this->email = $email;
@@ -18,27 +19,27 @@ class User
         $this->active = $active;
     }
 
-    public function getId() : int
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function getName() : string
+    public function getName(): string
     {
         return $this->name;
     }
 
-    public function getEmail() : string
+    public function getEmail(): string
     {
         return $this->email;
     }
 
-    public function getPassword() : string
+    public function getPassword(): string
     {
         return $this->password;
     }
 
-    public function getActive() : int
+    public function getActive(): int
     {
         return $this->active;
     }
@@ -46,12 +47,10 @@ class User
     public static function loggedIn()
     {
         if (!isset($_SESSION['usuario'])) {
-            header('Location: ../../../view/login.php');
+            header("Location: ../../../view/login.php");
             exit();
         } else {
             return $_SESSION['usuario'];
         }
     }
-
-    
 }
