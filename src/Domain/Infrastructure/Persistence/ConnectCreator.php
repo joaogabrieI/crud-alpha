3f5b63bb699d3fb3e1810826b33aac0cc1ef3e30
joaogabrieI[ <joaonalves1@gmail.com>
2024-03-07 11:30:15 -0300

    tratamento de erros conexão bd

--- src/Domain/Infrastructure/Persistence/ConnectCreator.php
+++ src/Domain/Infrastructure/Persistence/ConnectCreator.php
@@ -3,15 +3,20 @@
 namespace Alpha\Domain\Infrastructure\Persistence;
 
 use PDO;
+use PDOException;
 
 class ConnectCreator
 {
     public static function connect(): PDO
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=alpha', 'root' , 'admin');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, 1);
-
-        return $pdo;
+        try {
+            $pdo = new PDO('mysql:host=localhost;dbname=alpha', 'root', 'admin');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, 1);
+            return $pdo;
+        } catch (PDOException $e) {
+            echo "erro na conexao" . $e->getMessage();
+            die();
+        }
     }
 }
