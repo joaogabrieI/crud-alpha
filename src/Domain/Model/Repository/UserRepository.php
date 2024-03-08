6f746c0ba01ee6755872f3dbdec007176af98882
joaogabrieI[ <joaonalves1@gmail.com>
2024-03-08 11:42:08 -0300

    adição método findById

--- src/Domain/Model/Repository/UserRepository.php
+++ src/Domain/Model/Repository/UserRepository.php
@@ -7,8 +7,8 @@ use Alpha\Domain\Model\User;
 interface UserRepository
 {
     public function listAll() : array;
+    public function findById(int $id);
     public function save(User $user) : bool;
     public function remove(int $id) : void;
-
     public function update(User $user) : void;
 }
